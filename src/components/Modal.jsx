aa8f452313dd54f5cb7a99b7bb49f2d908a81fd6
Yaswanth Vemu <vemuyaswanth@gmail.com>
2021-10-01 18:56:42 +0530

    DEV-3.1

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,28 +1,34 @@
-import React from 'react'
-import '../css/modal.css'
+import React from "react";
+import "../css/modal.css";
 
-const Modal = () => {
+const Modal = ({ question, accept, decline, acceptFunc, declineFunc }) => {
   return (
     <>
       <div className="modal__main">
         <div className="modal__overlay">
           <div className="modal__window">
             <h3 className="modal__window-head">
-              Do you really want to Logout?
+              {question || "Do you really want to Continue?"}
             </h3>
             <div className="modal__window-buttons">
-              <button className="modal__window-buttons-decline modal-buttons">
-                No
+              <button
+                className="modal__window-buttons-decline modal-buttons"
+                onClick={declineFunc || null}
+              >
+                {decline || "No"}
               </button>
-              <button className="modal__window-buttons-accept modal-buttons">
-                Yeah
+              <button
+                className="modal__window-buttons-accept modal-buttons"
+                onClick={acceptFunc || null}
+              >
+                {accept || "Yes"}
               </button>
             </div>
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Modal
+export default Modal;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,34 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import Modal from './Modal'
+import { Modal } from "./";
 
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+toast.configure()
 const Navbar = ({ loginStatus, getDetails }) => {
+  const [logoutModalStatus, setLogoutModalStatus] = useState(false);
 
-  const [logoutModalStatus, setLogoutModalStatus] = useState(false)
-
-  const logout = (e) => {
-    e.preventDefault()
-    // localStorage.removeItem('token')
-    // window.location.replace('/')
-    setLogoutModalStatus(!logoutModalStatus)
+  function logout() {
+    setLogoutModalStatus(false);
+    toast("You are logged out now.", {
+      position: "bottom-right",
+      autoClose: 2000,
+      theme: "dark"
+    });
+    localStorage.removeItem('token')
+    setTimeout(() => {
+      window.location.replace('/')
+    }, 2000);
+  };
+  function cancelLogout() {
+    setLogoutModalStatus(false)
   }
-  
+  function showLogoutModal(e) {
+    e.preventDefault();
+    setLogoutModalStatus(true)
+  }
+
   return (
     <>
       <div className="navbar">
@@ -22,7 +38,7 @@ const Navbar = ({ loginStatus, getDetails }) => {
         <ul>
           {loginStatus ? (
             <>
-              <Link className="navbar__button" to="/" onClick={logout}>
+              <Link className="navbar__button" to="/" onClick={showLogoutModal}>
                 <button className="navbar__button-logout">Logout</button>
               </Link>
             </>
@@ -37,10 +53,15 @@ const Navbar = ({ loginStatus, getDetails }) => {
             </>
           )}
         </ul>
-        {/* <Modal /> */}
-        {
-          logoutModalStatus && <Modal />
-        }
+        {logoutModalStatus && (
+          <Modal
+            question="Do you really want to logout?"
+            decline="Cancel"
+            accept="Proceed"
+            declineFunc={cancelLogout}
+            acceptFunc={logout}
+          />
+        )}
       </div>
     </>
   );
